185454c722d2bb3dc9173b5f97a5739215791d79
Anelechukwu Jasper <jasperanels@gmail.com>
2025-06-21 18:56:50 +0100

    Added enemy actors

--- src/game_screen.c
+++ src/game_screen.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h> 
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include "application.h"
@@ -9,6 +11,12 @@
 
 #define BULLET_WIDTH 10
 #define BULLET_HEIGHT 10
+
+#define MOVE_ACTORS_TIME_LIMIT 60
+
+#define CREATE_ENEMY_ACTOR_TIME_LIMIT 600
+
+#define ENEMY_X_POSITIONS_SIZE 32
 
 int game_screen(square_application* application) {
 	Uint32 i;
@@ -41,9 +49,9 @@ int game_screen(square_application* application) {
 
 	int mouse_y = 0;
 	
-	Uint32 start_time = 0;
+	Uint32 move_actors_start_time = 0;
 
-	Uint32 current_time = 0;
+	Uint32 move_actors_current_time = 0;
 	
 	SDL_Rect* bullet_actors = NULL;
 
@@ -55,11 +63,40 @@ int game_screen(square_application* application) {
 
 	SDL_Rect shooter_actor = { SCREEN_WIDTH / 2, SCREEN_HEIGHT - (SHOOTER_HEIGHT * 2), SHOOTER_WIDTH, SHOOTER_HEIGHT };
 
+	int selected_enemy_actor;
+
+	unsigned int enemy_actor_x_positions[ENEMY_X_POSITIONS_SIZE] = { 
+		0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 
+		320, 340, 360, 380, 400, 420, 440, 460, 480, 500, 520, 540, 560, 580, 600, 620,
+	};
+
+	SDL_Rect* enemy_actors = NULL;
+
+	SDL_Rect* temp_enemy_actors = NULL;
+	
+	Uint32 enemy_actors_size = 0;
+
+	Uint32 create_enemy_actor_start_time = 0;
+
+	Uint32 create_enemy_actor_current_time = 0;
+
+
+	srand(time(NULL));
 
 	bullet_actors = malloc(sizeof(*bullet_actors));
 
 	if (bullet_actors == NULL) {
 		fprintf(application->log_file, "Error allocating bullet_actors");
+
+		return QUIT_SCREEN;
+	}
+
+	enemy_actors = malloc(sizeof(*enemy_actors));
+
+	if (enemy_actors == NULL) {
+		fprintf(application->log_file, "Error allocating enemy_actors");
+
+		free(bullet_actors);
 
 		return QUIT_SCREEN;
 	}
@@ -71,6 +108,7 @@ int game_screen(square_application* application) {
 		fprintf(application->log_file, "Unable to render back surface! SDL_ttf Error: %s\n", TTF_GetError());
 
 		free(bullet_actors);
+		free(enemy_actors);
 
 		return QUIT_SCREEN;
 	}
@@ -83,12 +121,15 @@ int game_screen(square_application* application) {
 		fprintf(application->log_file, "Unable to create texture from rendered back! SDL Error: %s\n", SDL_GetError());
 
 		free(bullet_actors);
+		free(enemy_actors);
 
 		return QUIT_SCREEN;
 	}
 
 
-	start_time = SDL_GetTicks();
+	move_actors_start_time = SDL_GetTicks();
+
+	create_enemy_actor_start_time = SDL_GetTicks();
 
 	
 	while (selected == 0) { 
@@ -111,7 +152,7 @@ int game_screen(square_application* application) {
 				}
 
 			} else if (event.type == SDL_KEYDOWN) {
-				
+
 				switch (event.key.keysym.sym) {
 					case SDLK_UP:
 						shooter_actor.y -= 4;
@@ -173,19 +214,49 @@ int game_screen(square_application* application) {
 		}
 
 
+		create_enemy_actor_current_time = SDL_GetTicks();
+
+		if ((create_enemy_actor_current_time - create_enemy_actor_start_time) >= CREATE_ENEMY_ACTOR_TIME_LIMIT) {
+			temp_enemy_actors = realloc(enemy_actors, (enemy_actors_size + 1) * sizeof(*enemy_actors));
+
+			if (temp_enemy_actors == NULL) {
+				fprintf(application->log_file, "Error re-allocating enemy_actors");
+			} else {
+				enemy_actors = temp_enemy_actors;
+
+				selected_enemy_actor = rand() % (ENEMY_X_POSITIONS_SIZE - 0) + 0;
+
+				enemy_actors[enemy_actors_size].y = header.h;
+				enemy_actors[enemy_actors_size].w = SHOOTER_WIDTH;
+				enemy_actors[enemy_actors_size].h = SHOOTER_HEIGHT;
+				enemy_actors[enemy_actors_size].x = enemy_actor_x_positions[selected_enemy_actor];
+
+				enemy_actors_size++;
+
+				// fprintf(application->log_file, "Enemy selected: %d & Index: %d\n", enemy_actors[enemy_actors_size - 1].x, selected_enemy_actor);
+				// fflush(application->log_file);
+			}
+		}
+
+		if ((create_enemy_actor_current_time - create_enemy_actor_start_time) >= CREATE_ENEMY_ACTOR_TIME_LIMIT) {
+			create_enemy_actor_start_time = create_enemy_actor_current_time;
+		}
+
+
 		SDL_SetRenderDrawColor(application->renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 
 		SDL_RenderClear(application->renderer);
 		
 		SDL_SetRenderDrawColor(application->renderer, 0x00, 0xFF, 0x00, 0xFF); 
 
-		current_time = SDL_GetTicks();
+
+		move_actors_current_time = SDL_GetTicks();
 
 
 		for (i = 0; i < bullet_actors_size; i++) {
 			SDL_RenderFillRect(application->renderer, &bullet_actors[i]);
 
-			if ((current_time - start_time) >= 60) {
+			if ((move_actors_current_time - move_actors_start_time) >= MOVE_ACTORS_TIME_LIMIT) {
 
 				if (bullet_actors[i].y <= 0) {
 					bullet_actors_size--;
@@ -199,8 +270,28 @@ int game_screen(square_application* application) {
 			}
 		}
 
-		if ((current_time - start_time) >= 60) {
-			start_time = current_time;
+
+		SDL_SetRenderDrawColor(application->renderer, 0xFF, 0xAB, 0x1D, 0xFF);   
+
+		for (i = 0; i < enemy_actors_size; i++) {
+			SDL_RenderFillRect(application->renderer, &enemy_actors[i]);
+
+			if ((move_actors_current_time - move_actors_start_time) >= MOVE_ACTORS_TIME_LIMIT) {
+
+				if (enemy_actors[i].y >= SCREEN_HEIGHT) {
+					enemy_actors_size--;
+
+					memset((void *) enemy_actors, 0, sizeof(*enemy_actors));
+
+					memmove((void *) enemy_actors, (void *) (enemy_actors + 1), enemy_actors_size * sizeof(*enemy_actors));
+				} else {
+					enemy_actors[i].y += 4;
+				}
+			}
+		}
+
+		if ((move_actors_current_time - move_actors_start_time) >= MOVE_ACTORS_TIME_LIMIT) {
+			move_actors_start_time = move_actors_current_time;
 		}
 
 		SDL_SetRenderDrawColor(application->renderer, 0xFF, 0x00, 0x00, 0xFF);        
@@ -248,8 +339,10 @@ int game_screen(square_application* application) {
 
 
 	free(bullet_actors);
+	free(enemy_actors);
 
 	SDL_DestroyTexture(score_texture);
+	SDL_DestroyTexture(back_texture);
 
 	return next_screen;
 }
